88ffa7c39a1486f0e412b24af4fdf14b33397690
Pink Moonlight <kate.cherie25@gmail.com>
2020-12-17 12:33:31 -0800

    completed projects Guess My Number, Modal and Pig Game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,78 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number ';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
+
+// For Test Only
+//document.querySelector('.number').textContent = secretNumber;
+
+//function to update message
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+// function to check the Players Guess
+let checkGuess = function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //console.log(guess, typeof guess);
+
+  // When no input
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+  } else if (guess > 20) {
+    displayMessage('⛔️ Number must be between 1 & 20!!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number');
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber ? '📈 Too high... ' : '📉 Too low... '
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+};
+
+// Event Listeners
+document.querySelector('.check').addEventListener('click', checkGuess);
+document.querySelector('body').addEventListener('keyup', function (e) {
+  if (e.code === 'Enter') {
+    checkGuess();
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+  displayMessage('Start guessing... ');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,100 @@
 'use strict';
+
+// Selecting DOM elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+let init = function () {
+  // set/reset state values
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  // reset UI
+  player0El.classList.remove('player--winner', 'name');
+  player1El.classList.remove('player--winner', 'name');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  // hide dice
+  diceEl.classList.add('hidden');
+};
+init();
+
+//  Switch players
+const nextPlayer = function () {
+  currentScore = 0;
+  document.getElementById(
+    `current--${activePlayer}`
+  ).textContent = currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// ***** EVENT HANDLERS *****
+// Rolling Dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //console.log(dice);
+
+    // 2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. check if rolled a 1, if true, switch to next player
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      //switch to next player
+      nextPlayer();
+    }
+  }
+});
+
+// Hold button functionality
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to total score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. check if total score is >= 100, Finish game, else switch player
+    if (scores[activePlayer] >= 10) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner', 'name') +
+        document
+          .querySelector(`.player--${activePlayer}`)
+          .classList.remove('player--active');
+      playing = false;
+      // could hide dice here if want, once winner declared
+    } else nextPlayer();
+  }
+});
+
+// 'New Game' button functionality
+btnNew.addEventListener('click', init);
